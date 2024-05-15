7742e9c54aecc578f0500947f115f2f8f7a91ea2
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-05-15 03:33:28 +0300

    adding ui et fix pilote

--- rally-qt/pilote.cpp
+++ rally-qt/pilote.cpp
@@ -1,3 +1,15 @@
 #include "pilote.h"
 
-Pilote::Pilote() {}
+// Constructeur
+Pilote::Pilote(int id_pilote, const std::string& nom_pilote, int id_categorie)
+    : id_pilote(id_pilote), nom_pilote(nom_pilote), id_categorie(id_categorie) {}
+
+// Accesseurs (getters)
+int Pilote::getIdPilote() const { return id_pilote; }
+const std::string& Pilote::getNomPilote() const { return nom_pilote; }
+int Pilote::getIdCategorie() const { return id_categorie; }
+
+// Mutateurs (setters)
+void Pilote::setIdPilote(int newIdPilote) { id_pilote = newIdPilote; }
+void Pilote::setNomPilote(const std::string& newNomPilote) { nom_pilote = newNomPilote; }
+void Pilote::setIdCategorie(int newIdCategorie) { id_categorie = newIdCategorie; }

0d126d2a2f5a956d297bd3c99849640afcdc0cfc
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-05-16 04:54:46 +0300

    udate  rep

---
+++ chrono.h
@@ -0,0 +1,34 @@
+#ifndef CHRONO_H
+#define CHRONO_H
+#include <jdbc/mysql_connection.h>
+#include<vector>
+class Chrono {
+private:
+    int id_special;
+    int id_pilote;
+    double temps;
+
+public:
+    // Constructeur
+    Chrono();
+    Chrono(int id_special, int id_pilote, double temps);
+
+    // Accesseurs (getters)
+    int getIdSpecial() const;
+    int getIdPilote() const;
+    double getTemps() const;
+
+    // Mutateurs (setters)
+    void setIdSpecial(int newIdSpecial);
+    void setIdPilote(int newIdPilote);
+    void setTemps(double newTemps);
+
+    //CRUD
+    void save();
+    static std::vector<Chrono> getAll();
+    static Chrono getById(int id);
+    void remove();
+    void update();
+};
+
+#endif // CHRONO_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
-#include <string>
-using namespace std;
+#include <jdbc/mysql_connection.h>
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/connection.h>
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/resultset.h>
+int main() {
+    // Création d'un objet de pilote MySQL
+    sql::Driver *driver;
+    driver = get_driver_instance();
 
-int main(){
-    cout << "Hello my friend" << endl;
-    string chaine;
-    getline(cin,chaine);
+    // Connexion à la base de données
+    sql::Connection *con;
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "jca");
+
+    // Sélection de la base de données
+    con->setSchema("Helicoptere");
+
+    // Exécution d'une requête SQL
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+    stmt = con->createStatement();
+    res = stmt->executeQuery("SELECT * FROM Obstacle");
+
+    // Parcourir et afficher les résultat
+
+    // Libération des ressources
+    delete res;
+    delete stmt;
+    delete con;
+
     return 0;
 }
